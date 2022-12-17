5902218d9858dee99b4fa87e71d5981cb2e400d0
Ira <43280192+Iraalza@users.noreply.github.com>
2022-12-17 23:14:01 +0300

    Работает!!!!!!!!!!!!!

---
+++ Auto.OwnerClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using Grpc.Net.Client;
+using Auto.OwnerServer;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7213");
+var grpcClient = new Owner.OwnerClient(channel);
+Console.WriteLine("Ready! Press any key to send a gRPC request (or Ctrl-C to quit).");
+while (true)
+{
+    Console.ReadKey(true);
+    var request = new OwnerByPhoneNumberRequest
+    {
+        Phonenumber = "74953286628"
+    };
+
+    var reply = grpcClient.GetOwner(request);
+    Console.WriteLine($"Price: {reply.Region}");
+}
---
+++ Auto.OwnerServer/Services/OwnerByPhoneNumberService.cs
@@ -0,0 +1,31 @@
+﻿using Grpc.Core;
+
+namespace Auto.OwnerServer.Services
+{
+    public class OwnerService : Owner.OwnerBase
+    {
+        private readonly ILogger<OwnerService> _logger;
+
+        public OwnerService(ILogger<OwnerService> logger)
+        {
+            this._logger = logger;
+        }
+
+        public override Task<OwnerByPhoneNumberResult> GetOwner(OwnerByPhoneNumberRequest request, ServerCallContext context)
+        {
+            string phonenumber = request.Phonenumber;
+            string region;
+            if (phonenumber[1] == '4' && phonenumber[2] == '9' && phonenumber[3] == '5')
+            {
+                region = "Moscow";
+            }
+            else
+            {
+                region = "Other";
+            }
+
+            return Task.FromResult(new OwnerByPhoneNumberResult() { Region = (string)region });
+
+        }
+    }
+}
